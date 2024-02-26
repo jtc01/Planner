cf558da1bad21c8d7d19552cf2145c66fe0ff7a1
jtc01 <joshcaopdx@gmail.com>
2024-02-26 08:57:59 -0800

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Joshua Cao on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
