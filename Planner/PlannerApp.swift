0b41ccac3aeb00892e82bde83fe66e4ec0cbe04b
jtc01 <69529420+jtc01@users.noreply.github.com>
2024-04-02 09:29:21 -0700

    Just did a bunch of stuff to register for notifications that I don't understand

--- Planner/PlannerApp.swift
+++ Planner/PlannerApp.swift
@@ -7,7 +7,27 @@
 
 import SwiftUI
 
+
+
 @main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            } else {
+                print("Permission for push notifications denied.")
+            }
+        }
+        return true
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print("Device Token: \(token)")
+    }
+}
 struct PlannerApp: App {
     var body: some Scene {
         WindowGroup {
